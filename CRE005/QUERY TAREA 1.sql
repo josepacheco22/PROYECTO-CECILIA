2ca60078ee3bd750e46b97eddce210a0c23a6329
JOSE <JOSE@DESKTOP-8T9DS95>
2022-05-05 11:05:42 -0500

    Agregar archivos de proyecto.

---
+++ CRE005/QUERY TAREA 1.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [EMPRESAS] (
+    [NUMERO_DOCUMENTO] varchar(100),
+    [PAIS] varchar(100),
+    [TIPO DOCUMENTO] varchar(100),
+    [EMPRESA] varchar(100),
+    [NATURAL_JURIDICA] varchar(100),
+)
